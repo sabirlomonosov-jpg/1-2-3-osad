3253761ba0c12ddbfe9b3baeea8c68a5653e2bf1
sabir <sabir.lomonosov@gmail.com>
2026-02-24 15:15:20 +0200

    Update_0.1

--- Kordamine/1-osa.cs
+++ Kordamine/1-osa.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+
+class Startclass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Tere tulemast!");
+        Console.Write("Sisesta eesnimi: ");
+        string eesnimi = Console.ReadLine();
+        Console.WriteLine("Tere, " + eesnimi);
+
+        int arv1 = ReadInt("Sisesta esimene arv: ");
+        int arv2 = ReadInt("Sisesta teine arv: ");
+        Console.WriteLine("Arvude {0} ja {1} korrutis võrdub {2}", arv1, arv2, arv1 * arv2);
+
+        Console.WriteLine("Vajuta Enter, et lõpetada...");
+        Console.ReadLine();
+    }
+
+    static int ReadInt(string prompt)
+    {
+        while (true)
+        {
+            Console.Write(prompt);
+            string? input = Console.ReadLine();
+            if (int.TryParse(input, out int value))
+                return value;
+            Console.WriteLine("Viga: sisesta kehtiv täisarv.");
+        }
+    }
+}
